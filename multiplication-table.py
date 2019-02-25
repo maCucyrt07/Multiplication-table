132d9710ebc58c799e8f2bbc70cebd4956a84046
maCucyrt07 <46973482+maCucyrt07@users.noreply.github.com>
2019-02-25 14:38:40 -0500

    Updated from Brython Server: 2/25/2019 2:38:37 PM

--- multiplication-table.py
+++ multiplication-table.py
@@ -1,8 +1,8 @@
 """
 multiplication-table.py
-Author: <your name here>
-Credit: <list sources used, if any>
-Assignment:
+Author: Macy
+Credit: Kyle
+Assignment: Multiplication Table
 
 Write and submit a Python program that prints a multiplication table. The user 
 must be prompted to give the width and height of the table before it is printed.
@@ -21,3 +21,10 @@ Height of multiplication table: 8
   7  14  21  28  35  42  49  56  63  70
   8  16  24  32  40  48  56  64  72  80
 """
+width = int(input("Width of multiplication table: "))
+height = int(input("Height of multiplication table: "))
+
+for i in range(1,(height+1)):
+    print("  ")
+    for j in range(1,width+1):
+        print(i*j,end="  ")
